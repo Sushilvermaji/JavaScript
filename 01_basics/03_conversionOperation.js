d53c05fc43c450250a0f6ff5df238cd2213cb3e4
Sushilvermaji <vsushil446@gmail.com>
2026-06-06 19:32:26 +0000

    type conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "345"
+
+console.log(typeof score)
+
+console.log(typeof (score))
+
+let valNum = Number(score)
+
+console.log(typeof valNum)
+
+// "33" => 33
+// "33acv" => NaN but its type a number 
+// true => 1 false
