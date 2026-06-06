120c86364fd9a14dadc3a4922cca44ecc693cffb
Sushilvermaji <vsushil446@gmail.com>
2026-06-06 19:15:15 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accId = 12234
+let accEmail = " Vsushil446@gmail.com"
+var accountPass = 23543
+
+// console.log(accId)
+
+console.table([accId,accEmail,accountPass])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert(3+3) we are using node js not browser
+
+let name = "Sushil Verma"
+let pass = 234
+
+// DATA TYPE 
+// number => 2 pwer 53
+// big int 
+// string 
+// boolean
+// null => standalone value
+// undefined 
+//symbol => uniqueness
+
+// object
+
+console.log(typeof name);
