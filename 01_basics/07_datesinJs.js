88f544b50918f12f84df20749df8e4a3a8fa435e
Sushilvermaji <vsushil446@gmail.com>
2026-06-09 13:22:18 +0000

    array start

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -2,12 +2,40 @@
 
 let myDate = new Date()
 
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toJSON());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
 
-console.log(typeof myDate);
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026 ,6, 9) // in this month start from 0 but in ddmm fprmat start from 1
+
+let myCreatedDate = new Date(2024,5,15,5,3)
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+
+newDate.toLocaleString('default',{
+    weekday : "long",
+})
+
+
+
+
+
+
+
 
 
 
---
+++ 02_basics/01_array.js
@@ -0,0 +1,27 @@
+// array
+
+const myArr = [0,1,2,3,4,5,true,"Verma"]
+
+const heroes = ["shaktiman" , "superman", "batman"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr[0]);
+
+// Array method
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(9)
+
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+
+
+console.log(myArr);
